3a15e3c415e9e3af57a8be7e3e60102fb8b9ba9f
Yauhescha <evgescha.maslov007@yandex.ru>
2023-06-14 20:11:35 +0300

    Initial commit

---
+++ android/src/com/hescha/jdigger/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.hescha.jdigger;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.hescha.jdigger.DiggerGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new DiggerGame(), config);
+	}
+}
